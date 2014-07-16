5dd2752de1d7473b07d35fc02de9c10ecef7cc3d
Me <quandary82@hailmail.net>
2014-07-16 13:59:36 +0200

    Really do mklink /j, not symlink creation

---
+++ mymklink/MbUnitFrameworkReplacement.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace MbUnit.FrameworkReplacement 
+{
+
+
+    internal class Assert
+    {
+
+        public static void IsTrue(bool b)
+        { }
+
+        public static void IsTrue(bool b, string Message)
+        { }
+
+        public static void IsFalse(bool b)
+        { }
+
+        public static void IsFalse(bool b, string Message)
+        { }
+
+        public static void AreEqual(object obj1, object obj2)
+        { }
+
+    }
+
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class TestAttribute : System.Attribute
+    { }
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class TestFixtureAttribute : System.Attribute
+    { }
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class SetUpAttribute : System.Attribute
+    { }
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class TearDownAttribute : System.Attribute
+    { }
+
+
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class TestsOnAttribute : System.Attribute
+    {
+        public TestsOnAttribute() { }
+        public TestsOnAttribute(Type t) { }
+    }
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class AuthorAttribute : System.Attribute
+    {
+        public AuthorAttribute() { }
+        public AuthorAttribute(string name, string email) { }
+    }
+
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class ExpectedExceptionAttribute : System.Attribute
+    {
+        public ExpectedExceptionAttribute() { }
+        public ExpectedExceptionAttribute(Type tException, string Message) { }
+    }
+
+
+}
--- mymklink/Program.cs
+++ mymklink/Program.cs
@@ -3,6 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 
+using System.IO;
+
+// http://community.bartdesmet.net/blogs/bart/archive/2006/10/24/Windows-Vista-_2D00_-Creating-symbolic-links-with-C_2300_.aspx
 
 namespace mymklink
 {
@@ -18,13 +21,75 @@ namespace mymklink
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
+            bool bShowWindow = false;
+
+            if (bShowWindow)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+
+            /*
+            //
+            // Symbolic file link bar.txt <<===>> foo.txt
+            //
+            string symF = "bar.txt";
+            string targetF = "foo.txt";
+
+            Console.WriteLine(">echo \"Hello World\" > {0}", targetF);
+            StreamWriter sw = File.CreateText(targetF);
+            sw.WriteLine("Hello World");
+            sw.Close();
+            Console.WriteLine();
+
+            Console.WriteLine(">mklink {0} {1}", symF, targetF);
+            if (mklink.CreateSymbolicLink(symF, targetF, 0))
+                Console.WriteLine("symbolic link created for {0} <<===>> {1}", symF, targetF);
+            Console.WriteLine();
+
+            Console.WriteLine(">type {0}", targetF);
+            Console.WriteLine(File.ReadAllText(targetF));
+            Console.WriteLine();
+
+            //
+            // Symbolic directory link bar <<===>> foo
+            //
+            string symD = "bar";
+            string targetD = "foo";
+
+            Console.WriteLine(">mkdir {0}", targetD);
+            Directory.CreateDirectory(targetD);
+            Console.WriteLine();
+
+            Console.WriteLine(">echo \"Hello World\" > {0}\\demo.txt", targetD);
+            StreamWriter sw2 = File.CreateText(targetD + "\\demo.txt");
+            sw2.WriteLine("Hello World");
+            sw2.Close();
+            Console.WriteLine();
+
+            Console.WriteLine(">mklink /d {0} {1}", symD, targetD);
+
+            if (mklink.CreateSymbolicLink(symD, targetD, mklink.SYMBOLIC_LINK_FLAG.Directory))
+                Console.WriteLine("symbolic link created for {0} <<===>> {1}", symD, targetD);
+            Console.WriteLine();
+
+            Console.WriteLine(">dir {0}", targetD);
+            foreach (string f in Directory.GetFiles(targetD))
+                Console.WriteLine(f);
+            */
+            //bool bbb = mklink.CreateSymbolicLink(@"d:\temp", @"D:\Temp2", mklink.SYMBOLIC_LINK_FLAG.File);
+            // Console.WriteLine(bbb);
+
+            // Symlink-name, TargetName, AllowOverwrite
+            FS.NTFS.JunctionPoint.Create(@"d:\temp2", @"D:\Temp", false);
+
+            
+
+        } // End Sub Main
 
 
-    }
+    } // End Class Program
 
 
-}
+} // End Namespace mymklink
